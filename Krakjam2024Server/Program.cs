92e7c1a1f4c20ffac61eb63802699f331fa0800b
Kamil Pastwa <kamil.pastwa@simfactor.pl>
2024-01-27 14:01:47 +0100

    init

---
+++ Krakjam2024Server/Program.cs
@@ -0,0 +1,35 @@
+namespace Placuszki.Krakjam2024.Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddRazorPages();
+            builder.Services.AddServerSideBlazor();
+            builder.Services.AddScoped<SessionService>();
+            builder.Services.AddSingleton<GameplayService>();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Error");
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+            app.UseRouting();
+
+            app.MapBlazorHub();
+            app.MapFallbackToPage("/_Host");
+            app.MapHub<GameHub>("/hubs/gamehub");
+
+            app.Run();
+        }
+    }
+}
