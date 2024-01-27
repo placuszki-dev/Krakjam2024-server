51325383cd9ecbad8b316b19c0847c7e7644d075
Kamil Pastwa <kamil.pastwa@simfactor.pl>
2024-01-27 14:29:22 +0100

    feat: x, y, phone color

--- Krakjam2024Server/DataModel/DataPacket.cs
+++ Krakjam2024Server/DataModel/DataPacket.cs
@@ -1,12 +1,11 @@
-﻿using System;
-
-namespace Placuszki.Krakjam2024.Server
+﻿namespace Placuszki.Krakjam2024.Server
 {
     [Serializable]
     public class DataPacket
     {
         public string PlayerId { get; set; }
-        public string Key { get; set; }
-        public int Value { get; set; }
+        public string PhoneColor { get; set; }
+        public float X { get; set; }
+        public float Y { get; set; }
     }
 }
--- Krakjam2024Server/Gameplay/SessionService.cs
+++ Krakjam2024Server/Gameplay/SessionService.cs
@@ -3,4 +3,15 @@
 public class SessionService
 {
     public string PlayerId { get; set; } = Guid.NewGuid().ToString();
+    public string PhoneColor { get; set; } = GenerateRandomHexColor();
+
+    private static string GenerateRandomHexColor()
+    {
+        Random random = new Random();
+        byte[] colorBytes = new byte[3];
+        random.NextBytes(colorBytes);
+
+        string hexColor = $"#{colorBytes[0]:X2}{colorBytes[1]:X2}{colorBytes[2]:X2}";
+        return hexColor;
+    }
 }
