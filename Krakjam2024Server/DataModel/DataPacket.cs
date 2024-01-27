2a71785b40103f73b2a83bf1354a0a833061bee3
Pastew <kpastew@gmail.com>
2024-01-27 23:50:33 +0100

    ODDAJCIE PLAYER ID

--- Krakjam2024Server/DataModel/DataPacket.cs
+++ Krakjam2024Server/DataModel/DataPacket.cs
@@ -5,6 +5,7 @@ namespace Placuszki.Krakjam2024.Server
     [Serializable]
     public class DataPacket
     {
+        public string PlayerId { get; set; }
         public float X { get; set; }
         public float Y { get; set; }
     }
