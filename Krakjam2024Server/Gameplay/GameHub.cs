5f09f1ec936c8c29f7fd9cdca5a7caa82660f4e5
Pastew <kpastew@gmail.com>
2024-01-28 01:10:54 +0100

    feat: end game from user info

--- Krakjam2024Server/Gameplay/GameHub.cs
+++ Krakjam2024Server/Gameplay/GameHub.cs
@@ -1,17 +1,26 @@
-﻿using Microsoft.AspNetCore.SignalR;
+﻿using CargoAR.Shared.Network;
+using Microsoft.AspNetCore.SignalR;
 
 namespace Placuszki.Krakjam2024.Server;
 
 // GameHub is created at each message received from client or sent to client
-public class GameHub : Hub<IGameHub>
+public class GameHub : Hub<IGameHub>, IClientHub
 {
+    private readonly GameplayService _gameplayService;
+
     public GameHub(GameplayService gameplayService)
     {
+        _gameplayService = gameplayService;
     }
 
     public override Task OnConnectedAsync()
     {
         Console.WriteLine($"New client connected. Sending initial data");
         return base.OnConnectedAsync();
+    }
+
+    public void SendEndGameToServer(UserInfo winner)
+    {
+        _gameplayService.OnEndGameReceivedFromClient(winner);
     }
 }
